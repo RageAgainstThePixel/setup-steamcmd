28efd709b71f9bd0d5c89539aa328a7694bad34e
Stephen Hodgson <hodgson.designs@gmail.com>
2024-07-25 13:12:49 -0400

    dist

--- dist/index.js
+++ dist/index.js
@@ -33327,7 +33327,7 @@ function getExecutable(directory) {
 }
 
 async function getVersion(path) {
-    const semVerRegEx = '\\(c\\) Valve Corporation - version (?<version>\\d+)';
+    const semVerRegEx = 'Steam Console Client \\(c\\) Valve Corporation - version (?<version>\\d+)';
     let output = '';
     await exec.exec(path, ['version', '+quit'], {
         listeners: {
